fd59b743faa1ad2b64b5e965deabab245d42bd52
Georgi Karchev <georgi.karchev@gmail.com>
2023-02-04 23:06:33 +0200

    Add urlencoded

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 const config = {
